82d014048d9bdb5c1964756d76e064bf0cccfc79
mariaavitoriarodrigues <maria.silva.86@academico.ifpb.edu.br>
2025-08-14 14:21:48 -0300

    questoes resolvidas

--- fundamentos-questao1.js
+++ fundamentos-questao1.js
@@ -1,7 +1,6 @@
-//Exibe dia da semana e hora atual
 
 const hoje = new Date();
 const diasSemana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
-console.log("Hoje é :$ {quinta-feira}[hoje.getDay()].");
-console.log("A hora atual é :$ {hoje.toLocaleTimeString("pt-BR")}");
 
+console.log(`Hoje é: ${diasSemana[hoje.getDay()]}.`);
+console.log(`A hora atual é: ${hoje.toLocaleTimeString("pt-BR")}`);
